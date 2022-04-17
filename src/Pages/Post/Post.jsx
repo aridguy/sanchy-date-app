5c1346673350ca9084ea8bc55f9857a927abfea3
Ayoola Taiwo <tayoola13@yahoo.com>
2022-04-17 22:29:14 +0100

    Removed unused declarations in the Post page

--- src/Pages/Post/Post.jsx
+++ src/Pages/Post/Post.jsx
@@ -3,16 +3,10 @@ import './Post.css'
 import './PostMobile.css'
 import LoggedInHeader from '../../components/LoggedInHeader/LoggedInHeader'
 import Sidebar from '../../components/Sidebar/Sidebar'
-import { BsArrowLeft, BsCameraVideo } from 'react-icons/bs';
-import { MdLocationPin, MdOutlineMessage } from 'react-icons/md';
-import { VscDeviceCamera } from 'react-icons/vsc';
-import midImgOne from '../../assets/images/prof-placeholder-one.png'
-import midImgTwo from '../../assets/images/prof-placeholder-two.png'
+import { BsArrowLeft } from 'react-icons/bs';
 
 
 // import sidenav_profile from "../../assets/images/sidenav-profile.svg";
-import ProfileAboutSection from '../../components/ProfileAboutSection/ProfileAboutSection';
-import UserContentSection from '../../components/UserContentSection/UserContentSection';
 import HotpicksHolder from '../../components/HotpicksHolder/HotpicksHolder';
 
 import hookup1 from "../../assets/images/hookup1.svg";
